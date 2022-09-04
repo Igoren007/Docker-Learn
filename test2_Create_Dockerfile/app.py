30e8b78656cbdb7a6424944335c9dafa6363881a
Igor Garmatenko <igoren92@gmail.com>
2022-09-05 01:45:28 +0300

    modify test2/app.py

--- test2_Create_Dockerfile/app.py
+++ test2_Create_Dockerfile/app.py
@@ -1,5 +1,11 @@
+from email import message
 import time
+import datetime
 
 while True:
-    print('Hello from container')
+    now = datetime.datetime.now()
+    message = f'Hello from Python at {now.strftime("%d-%m-%Y %H:%M:%S")}'
+    print(message)
+    with open("/usr/src/data/output.txt", "a") as file:
+        file.write(message)
     time.sleep(3)
